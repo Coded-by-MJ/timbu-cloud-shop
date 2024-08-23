297f44c404bc934051f338f5b91dc928a8456bf7
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-08-23 13:06:58 +0100

    updated Images

--- src/components/productImages.jsx
+++ src/components/productImages.jsx
@@ -1,14 +1,14 @@
 
 
 export const productImages  = {
-    'Autume Dress' :  '../src/assets/images/autume.png',
-    'Slender Gown' :  '../src/assets/images/gown.png', 
-    'Fanel Turtleneck' :  '../src/assets/images/fanel.png',
-    'Autume Jumper' :  '../src/assets/images/jumper.png', 
-    'NeatNexus' :  '../src/assets/images/neatNexus.png',
-    'Office Runway' :  '../src/assets/images/office.png', 
-    'Classic Mens Combo' :  '../src/assets/images/classic.png',
-    'SleekStyle Couture' :  '../src/assets/images/sleekStyle.png',  
-    'Turtleneck Givenchy' :  '../src/assets/images/givenchy.png',
+    'Autume Dress' :  '/images/autume.png',
+    'Slender Gown' :  '/images/gown.png', 
+    'Fanel Turtleneck' :  '/images/fanel.png',
+    'Autume Jumper' :  '/images/jumper.png', 
+    'NeatNexus' :  '/images/neatNexus.png',
+    'Office Runway' :  '/images/office.png', 
+    'Classic Mens Combo' :  '/images/classic.png',
+    'SleekStyle Couture' :  '/images/sleekStyle.png',  
+    'Turtleneck Givenchy' :  '/images/givenchy.png',
 }
       
