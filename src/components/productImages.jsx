bddafb30751e8b3228dbfac11c8136e094edd520
Miracle Ibharokhonre <miracleosemudiahen@hotmail.com>
2024-08-23 14:54:07 +0100

    updated Images

--- src/components/productImages.jsx
+++ src/components/productImages.jsx
@@ -1,14 +1,14 @@
 
 
 export const productImages  = {
-    'Autume Dress' :  '/images/autume.svg',
-    'Slender Gown' :  '/images/gown.svg', 
-    'Fanel Turtleneck' :  '/images/fanel.svg',
-    'Autume Jumper' :  '/images/jumper.svg', 
-    'NeatNexus' :  '/images/neatNexus.svg',
-    'Office Runway' :  '/images/office.svg', 
-    'Classic Mens Combo' :  '/images/classic.svg',
-    'SleekStyle Couture' :  '/images/sleekStyle.svg',  
-    'Turtleneck Givenchy' :  '/images/givenchy.svg',
+    'Autume Dress' :  '/images/autume.webp',
+    'Slender Gown' :  '/images/gown.webp', 
+    'Fanel Turtleneck' :  '/images/fanel.webp',
+    'Autume Jumper' :  '/images/jumper.webp', 
+    'NeatNexus' :  '/images/neatNexus.webp',
+    'Office Runway' :  '/images/office.webp', 
+    'Classic Mens Combo' :  '/images/classic.webp',
+    'SleekStyle Couture' :  '/images/sleekStyle.webp',  
+    'Turtleneck Givenchy' :  '/images/givenchy.webp',
 }
       
